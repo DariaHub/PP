d34a07f029b12fca880e64fb36a0f46a5caacb77
pomel <pomelova.dasha@mail.ru>
2022-10-10 20:48:05 +0300

    ЛР2

---
+++ ИщуIT/Repository/ItCompanyRepository.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ItCompanyRepository : RepositoryBase<ItCompany>, IItCompanyRepository
+    {
+        public ItCompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+    }
+}
---
+++ ИщуIT/Repository/VacancyRepository.cs
@@ -0,0 +1,18 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class VacancyRepository : RepositoryBase<Vacancy>, IVacancyRepository
+    {
+        public VacancyRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+    }
+}
