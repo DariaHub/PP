2e5a16548a2b19f4f634ae0ec9e00c54b90d0235
pomel <pomelova.dasha@mail.ru>
2022-10-13 21:26:57 +0300

    лр3

---
+++ ИЩУIT/Repository/ItCompanyRepository.cs
@@ -0,0 +1,21 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ItCompanyRepository : RepositoryBase<ItCompany>, IItCompanyRepository
+    {
+        public ItCompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+
+        public IEnumerable<ItCompany> GetAllItCompanies(bool trackChanges) =>
+            FindAll(trackChanges).OrderBy(c => c.Name).ToList();
+    }
+}
